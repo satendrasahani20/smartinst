183b67b2891dffa8ca769a788dca07bcdc3417a4
Satendra Sahani <satendrasahani19@gmail.com>
2023-05-30 11:11:51 -1200

    change instance

--- src/service/api/apiInstance.js
+++ src/service/api/apiInstance.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const baseUrl = () => {
-  let base_url = "http://localhost:8000/"
-  //   if(window?.location?.href?.includes("https://dev")) {
-  //     base_url = "https://dev.olibr.com/api/v1"
-  //   } else if(window?.location?.href?.includes("https://test")) {
+  let base_url = "https://localhost:3000"
+    if(window?.location?.href?.includes("https://green-gosling-boot.cyclic.app")) {
+      base_url = "https://green-gosling-boot.cyclic.app"
+    } 
+  // else if(window?.location?.href?.includes("https://test")) {
   //     base_url = "https://test.olibr.com/api/v1"
   //   } else if(window?.location?.href?.includes("https://olibr")) {
   //     base_url = "https://olibr.com/api/v1"
